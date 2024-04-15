8967cc8d98704810790f4850910873c2fc7eb8b0
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-04-15 20:34:06 +0530

    Update store.js

--- pages/store.js
+++ pages/store.js
@@ -4,13 +4,12 @@ import {useRouter} from 'next/navigation'
 function Store() {
   const router = useRouter();
 
-  
   useEffect(()=>{
     const isLoggedIn = localStorage.getItem('isLoggedIn');
     if(isLoggedIn === "false"){
       router.push('/login');
     }
-  } , [])
+  },[])
 
   function handleLogout(){
     localStorage.setItem('isLoggedIn', JSON.stringify(false))
