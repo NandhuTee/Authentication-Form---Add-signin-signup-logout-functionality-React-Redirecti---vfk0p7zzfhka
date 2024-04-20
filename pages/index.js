84cd04cc7438b3431be8973b730f091be44b0923
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-04-20 12:00:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import Link from 'next/link';
-import {useRouter} from 'next/navigation'
+import { useRouter } from 'next/navigation';
 
 function Register() {
   const router = useRouter();
@@ -10,40 +10,56 @@ function Register() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState(false);
 
-  function handleRegisterUser(event){
+  function handleRegisterUser(event) {
     event.preventDefault();
-    if(email.length <= 0 || password.length <= 0 ){
+    // Validate input
+    if (email.length === 0 || password.length === 0) {
       setError(true);
       return;
     }
-    localStorage.setItem('user', JSON.stringify({email, password}));
+
+    // Store email and password in local storage
+    localStorage.setItem('user', JSON.stringify({ email, password }));
+
+    // Clear input fields
     setEmail('');
     setPassword('');
-    router.push('/login')
 
+    // Redirect to login page
+    router.push('/login');
   }
 
   return (
-    <div className='register'>
+    <div className="register">
       <h2>Register</h2>
-      <form className='sign-up-form' onSubmit={handleRegisterUser}>
-        {error ? <p className='error-para'>"Email or password isn't entered!"</p>: null}
-        <div className='email-div'>
-          <label htmlFor='email'>Email: </label>
-          <input type='email' id='email' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+      <form className="sign-up-form" onSubmit={handleRegisterUser}>
+        {error && <p className="error-para">"Email or password isn't entered!"</p>}
+        <div className="email-div">
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-        <div className='password-div'>
-          <label htmlFor='password'>Password: </label>
-          <input type='password' id='password' value={password} onChange={(e)=> setPassword(e.target.value)}/>
+        <div className="password-div">
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <button type='submit' className='register-btn'>
+        <button type="submit" className="register-btn">
           Register
         </button>
       </form>
       <div>
         Already have an account?
-        <Link href='/login'>
-          <button className='login-link'>Login</button>
+        <Link href="/login">
+          <button className="login-link">Login</button>
         </Link>
       </div>
     </div>
